6b767451bdd9bfbae800cfb15c04192118d5dc98
Charles Ford <charles.ford1978@gmail.com>
2022-01-26 16:11:01 -0500

    Add project files.

---
+++ HackerRankStaircaseProblem/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        string space = " ";
+        string hash = "#";
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            int jcount = 0;
+
+            for (int j = i; j < n; j++)
+            {
+                jcount++;
+                Console.Write(space);
+            }
+            for (int k = n; k > jcount; k--)
+            {
+                Console.Write(hash);
+            }
+            Console.WriteLine("");
+        }
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
